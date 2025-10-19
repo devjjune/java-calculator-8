5fcceeef269847566c342be81ce5df899dfab9db
황지윤 <hwangjiyoon@hwangjiyun-ui-noteubug.local>
2025-10-19 18:08:34 +0900

    feat: InputHandler 클래스 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        String input = Console.readLine();
+
 
         if (input.startsWith("//")) {
             int a = input.indexOf("\\n");
--- src/main/java/calculator/InputHandler.java
+++ src/main/java/calculator/InputHandler.java
@@ -1,2 +1,12 @@
-package calculator;public class InputHandler {
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputHandler {
+
+    public static String readInput() {
+        String input = Console.readLine();
+        return input;
+    }
+
 }
