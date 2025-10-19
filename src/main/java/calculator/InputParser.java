1e0e457c26fbb0af25e36b76ed8c27b13cd57468
황지윤 <hwangjiyoon@hwangjiyun-ui-noteubug.local>
2025-10-19 18:34:45 +0900

    feat: InputParser 클래스 추가

---
+++ src/main/java/calculator/InputParser.java
@@ -0,0 +1,23 @@
+package calculator;
+
+public class InputParser {
+
+    public static String[] parse(String input) {
+        if (input.startsWith("//")) { // 입력이 커스텀 구분자로 시작하는 경우
+            int a = input.indexOf("\\n");
+            if (a == -1) {
+                a = input.indexOf("\n");
+            }
+
+            String i = input.substring(2, a);
+            String numbers = input.substring(a + 2);
+
+            return numbers.split("[,:]|" + i);
+
+        }
+
+        return input.split("[,:]");
+
+    }
+
+}
